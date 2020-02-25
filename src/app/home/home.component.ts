1fc47ae24b95fbc4ed474f4129e7daea1169b745
Yeltsin <yel.ruben@gmail.com>
2020-02-25 14:39:13 -0430

    fix flechas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,41 +25,23 @@ export class HomeComponent implements OnInit {
   
   ngOnInit(): void {
    
-    this.fetchNews();
+    
     this.fetchUsers();
     $('.carousel').carousel('pause');
   }
 
   ngAfterViewInit(){
   
-    this.carrousel();
+   
     $('.carousel').carousel('pause');
-    //$("#owl-demo").appendTo(document.getElementById("items"));
+  
    
   }
   ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
+    
     
   }
-  fetchNews(){
-    this.resultNewsService.getAllNews()
-    .subscribe(result => {
-      this.ResultNews = result;
-      
-      this.news=this.ResultNews["articles"];
-   
-      let string:string;
-      for(let i=0;i<=this.news.length -1;i++){
-        string += '<div class="card"><div class="card-body"><img style="width: 254px!important;height: 255px!important;" src="../../assets/icon-2.png" alt=""> <div class="card-title"><p >Great <strong>Communication</strong></p></div><p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p></div></div>';
-        
-      }
-     string = string.replace('undefined','');
-     $('.documentation').append(string);
-     
-    });
-    
-  }
+  
 
   fetchUsers(){
     this.usersService.getAllUsers()
@@ -71,27 +53,5 @@ export class HomeComponent implements OnInit {
     });
     
   }
-  carrousel(){
-    
-    setTimeout(function () {
-      $('.documentation').owlCarousel({
-        loop: true,
-        margin: 10,
-         nav: false,
-        responsive: {
-          0: {
-            items: 1
-          },
-          600: {
-            items: 2
-          },
-          1000: {
-            items: 5
-          }
-        }
-
-      });
-    }, 3000);
-  }
-
+ 
 }
